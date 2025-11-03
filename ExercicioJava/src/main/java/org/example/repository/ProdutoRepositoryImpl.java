4ed4971c9ea6783643e025abfee26ca02b4b359d
profviniciustrindade <vinicius.trindade@edu.sc.senai.br>
2025-11-03 14:28:01 -0300

    Adiciona implementação do repositório

---
+++ ExercicioJava/src/main/java/org/example/repository/ProdutoRepositoryImpl.java
@@ -0,0 +1,32 @@
+package org.example.repository;
+
+import org.example.model.Produto;
+
+import java.util.List;
+
+public class ProdutoRepositoryImpl implements ProdutoRepository{
+    @Override
+    public Produto save(Produto produto) {
+        return null;
+    }
+
+    @Override
+    public List<Produto> findAll() {
+        return null;
+    }
+
+    @Override
+    public Produto findById() {
+        return null;
+    }
+
+    @Override
+    public Produto update(Produto produto) {
+        return null;
+    }
+
+    @Override
+    public void deleteById(int id) {
+
+    }
+}
