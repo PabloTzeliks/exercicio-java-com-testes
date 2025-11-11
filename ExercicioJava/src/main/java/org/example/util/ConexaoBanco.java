c82ab64598b8b37b438b76bf3210569d659802f3
PabloTzeliks <arq.pabloo@gmail.com>
2025-11-11 15:39:08 -0300

    feat: implement the persistency connection for database on the 'ConexaoBanco' class

--- ExercicioJava/src/main/java/org/example/util/ConexaoBanco.java
+++ ExercicioJava/src/main/java/org/example/util/ConexaoBanco.java
@@ -1,10 +1,16 @@
 package org.example.util;
 
 import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
 
 public class ConexaoBanco {
 
-    public static Connection conectar(){
-        return null;
+    private static final String URL = "jdbc:mysql://localhost:3306/sql_db?useSSL=false&serverTimezone=UTC";
+    private static final String USER = "root";
+    private static final String PASSWORD = "passwd";
+
+    public static Connection conectar() throws SQLException {
+        return DriverManager.getConnection(URL, USER, PASSWORD);
     }
 }
